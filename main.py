81dbbf1dd2c7683c6268d81fb591234afc23d118
skylineone044 <skylineone044@gmail.com>
2022-08-07 00:47:13 +0200

    load ui file

---
+++ main.py
@@ -0,0 +1,42 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile, QIODevice
+from PyQt6 import uic
+
+import sys
+
+
+class RouteWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("RouteWidget.ui", self)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Simple App Audio Router")
+        r1 = RouteWidget()
+
+        # Set the central widget of the Window.
+        main_widget = QWidget()
+        vbox = QVBoxLayout()
+        vbox.addChildWidget(r1)
+        main_widget.setLayout(vbox)
+        self.setCentralWidget(main_widget)
+
+
+app = QApplication(sys.argv)
+
+from PyQt6.QtWidgets import QStyleFactory
+
+AVAILABLE_THEMES = QStyleFactory.keys()
+print(f"Available themes: {AVAILABLE_THEMES}")
+app.setStyle(AVAILABLE_THEMES[0])
+print(f"Current theme: {app.style().objectName()}")
+
+window = MainWindow()
+window.show()
+
+app.exec()
