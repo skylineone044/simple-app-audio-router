8045a18f6e86268452e6eda41eac09c7ce2995ca
skylineone044 <skylineone044@gmail.com>
2022-08-09 14:16:14 +0200

    figured out the routewidget layout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QFrame
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile, QIODevice
-from PyQt6 import QtWidgets, uic
+from PyQt6 import QtWidgets, uic, QtCore
 
 import sys
 
@@ -15,7 +15,7 @@ class RouteWidget(QWidget):
         uic.loadUi("RouteWidget.ui", self)
         self.app_output_comboboxes = []
 
-        self.addMoreAppsButton.clicked.connect(self.add_app_output_combobox)
+        self.add_more_apps_btn.clicked.connect(self.add_app_output_combobox)
         self.removeSinkButton.clicked.connect(self.remove)
 
         self.add_app_output_combobox()
@@ -25,30 +25,49 @@ class RouteWidget(QWidget):
         cb.addItems(
             [" "] + [f"{item_id}: {item_name}" for item_id, item_name in pw_interface.get_node_outputs().items()])
 
+    def remove_app_output_combobox(self, cb):
+        vpadding = 25
+        # TODO: do not forget to disconnect the app from the virtual sink
+        self.app_output_comboboxes.remove(cb)
+        cb.setParent(None)
+
+        self.app_list.setFixedHeight(self.app_list_vbox.sizeHint().height() + vpadding)
+        self.setFixedHeight(max(self.app_list_vbox.sizeHint().height() + vpadding, 80))
+
+
     def add_app_output_combobox(self):
         hbox = QFrame()
+        hbox.setContentsMargins(0, 0, 0, 0)
+        height = 32
+
+        self.app_list_vbox.addWidget(hbox, alignment=QtCore.Qt.AlignmentFlag.AlignTop)
+        self.app_output_comboboxes.append(hbox)
+
         hbox_layout = QHBoxLayout()
         hbox.setLayout(hbox_layout)
+        hbox.setFixedHeight(height)
+
         cb = combobox.ComboBox()
-        cb.setFixedHeight(24)
+        cb.setFixedHeight(height - 7)
+        cb.popupAboutToBeShown.connect(
+            lambda: self.update_app_selection_combobox_items(cb))
+
         remove_btn = QPushButton("Remove")
-        remove_btn.setFixedHeight(24)
+        remove_btn.setFixedHeight(height - 7)
         remove_btn.setFixedWidth(60)
-        hbox.setFixedHeight(30)
+        remove_btn.clicked.connect(lambda: self.remove_app_output_combobox(hbox))
 
         hbox_layout.addWidget(cb)
         hbox_layout.addWidget(remove_btn)
 
-        self.appOutputs.addWidget(hbox)
-        self.setMinimumHeight(max(self.verticalLayout.sizeHint().height() + 50, 120))
+        vpadding = 10
+        self.app_list.setFixedHeight(len(self.app_output_comboboxes) * height + vpadding)
+        self.setFixedHeight(max(len(self.app_output_comboboxes) * height + 2 * vpadding, 100))
 
-        cb.popupAboutToBeShown.connect(
-            lambda: self.update_app_selection_combobox_items(cb))
-        self.app_output_comboboxes.append(hbox)
 
 
     def remove(self):
-        ## do not forget to close the sink for this widget as well
+        # TODO: do not forget to close the sink for this widget as well
         self.setParent(None)
 
 
@@ -64,7 +83,7 @@ class MainWindow(QMainWindow):
 
     def add_router_widget(self):
         self.routerWidgets.append(RouteWidget())
-        self.output_list.addWidget(self.routerWidgets[-1])
+        self.output_list.addWidget(self.routerWidgets[-1], alignment=QtCore.Qt.AlignmentFlag.AlignTop)
 
 
 app = QApplication(sys.argv)
